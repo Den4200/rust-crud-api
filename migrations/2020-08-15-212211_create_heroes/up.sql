1dbf88e759cef8ab8b25bc9ae5d3b11ce83bd67a
Den4200 <dennis@dennispham.me>
2020-08-15 17:34:45 -0400

    Install diesel and create inital migration

---
+++ migrations/2020-08-15-212211_create_heroes/up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE heroes (
+    id INT(11) PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    identity VARCHAR(60) NOT NULL,
+    hometown VARCHAR(60) NOT NULL,
+    age INT(11) NOT NULL
+
+)
---
+++ src/schema.rs
@@ -0,0 +1,9 @@
+table! {
+    heroes (id) {
+        id -> Nullable<Integer>,
+        name -> Text,
+        identity -> Text,
+        hometown -> Text,
+        age -> Integer,
+    }
+}
