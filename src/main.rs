054c64d974977d084b4e4a39cbfb72b1085d20dc
Den4200 <dennis@dennispham.me>
2020-08-15 15:24:17 -0400

    Add simple hello route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {}. You are {} years old.", name, age)
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/hello", routes![hello])
+        .launch();
 }
